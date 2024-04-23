aee04aab3ab6cc4ef28fe321cd82dd71d78bd9be
Pedro Augusto Laguna <lagunaaustore@gmail.com>
2024-04-23 16:48:56 -0400

    Completing task9

---
+++ 9-element_length.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+element_length - takes a list element as argument and returns the length of the element
+"""
+
+from typing import Sequence, Union, Any
+from typing import List, Tuple
+
+def element_length(lst: Sequence[Union[int, str, Tuple[Any], List[Any]]]) -> List[int]:
+    """
+    element_length - takes a list element as argument and returns the length of the element
+    lst: list of integers, strings, tuples, and lists
+    returns: list of integers representing the length of each element in lst
+    """
+    return [len(i) for i in lst]
