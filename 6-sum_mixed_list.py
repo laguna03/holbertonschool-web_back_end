9a73f695c4bfbfbd9b20dbf2e1e6e5e3013335e1
Pedro Augusto Laguna <lagunaaustore@gmail.com>
2024-04-23 16:41:54 -0400

    sum of mixed list named mxd_list as a float

---
+++ 6-sum_mixed_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+sum_mixed_list - takes a list mxd_lst of integers and floats and returns their sum as a float
+"""
+
+from typing import List, Union
+
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
+    """
+    sum_mixed_list - takes a list mxd_lst of integers and floats and returns their sum as a float
+    mxd_lst: list of integers and floats
+    returns: sum of mxd_lst as a float
+    """
+    return sum(mxd_lst)
