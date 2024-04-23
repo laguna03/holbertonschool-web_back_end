c877bb2cffcd39e009523a74bfc7f0df5f268635
Pedro Augusto Laguna <lagunaaustore@gmail.com>
2024-04-23 16:32:39 -0400

    Sum of list of floats in type annotated

---
+++ 5-sum_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Function that takes a list of floats as argument and returns their sum as a float in type-annotated form
+"""
+
+from typing import List
+
+def sum_list(input_list: List[float]) -> float:
+    """
+    Function that takes a list of floats as argument and returns their sum as a float in type-annotated form
+    """
+    return sum(input_list)
