dc451e19ab425d987a00b5d8854c99243b2b778b
Pedro Augusto Laguna <lagunaaustore@gmail.com>
2024-04-23 16:20:20 -0400

    Round a flout in type annotated

---
+++ 2-floor.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Module to round a float in type-annotated fashion.
+"""
+
+import math
+
+def floor(n: float) -> int:
+    """
+    Function that rounds a float in type-annotated fashion.
+    """
+    return math.floor(n)
