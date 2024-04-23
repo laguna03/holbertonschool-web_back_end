b1de9394193e9c56159517a6c3410d832c2d2546
Pedro Augusto Laguna <lagunaaustore@gmail.com>
2024-04-23 16:22:07 -0400

    Float to str

---
+++ 3-to_str.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+Module to convert a float to a string in type-annotated fashion.
+"""
+
+def to_str(n: float) -> str:
+    """
+    Function that converts a float to a string in type-annotated fashion.
+    """
+    return str(n)
