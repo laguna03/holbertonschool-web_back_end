21bffdf384a8755299c24f6139149072e6e5991e
Pedro Augusto Laguna <lagunaaustore@gmail.com>
2024-04-23 16:09:05 -0400

    first commit

---
+++ 0-add.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+This module contains a function that adds two floats in type-annotated fashion.
+"""
+
+def add(a: float, b: float) -> float:
+    """
+    Function that adds two floats in type-annotated fashion.
+    """
+    return a + b
