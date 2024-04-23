d06f8503398275e9eb27acb2a0fc1f146629f3b2
Pedro Augusto Laguna <lagunaaustore@gmail.com>
2024-04-23 16:17:58 -0400

    concat in type annotation

---
+++ 1-concat.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+Module to concatenate two strings in type-annotated fashion.
+"""
+
+def concat(str1: str, str2: str) -> str:
+    """
+    Function that concatenates two strings in type-annotated fashion.
+    """
+    return str1 + str2
