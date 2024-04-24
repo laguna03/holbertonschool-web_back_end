84d2da20568b463e7b1f4c6276f8e791b8832b1a
Pedro Augusto Laguna <lagunaaustore@gmail.com>
2024-04-24 10:56:32 -0400

    Fixing file errors

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -3,6 +3,7 @@
 This module contains a function that adds two floats in type-annotated fashion.
 """
 
+
 def add(a: float, b: float) -> float:
     """
     Function that adds two floats in type-annotated fashion.
