4252f99359bfd6324baf03f3e3957468acf40e3f
Pedro Augusto Laguna <lagunaaustore@gmail.com>
2024-04-25 18:49:39 -0400

    Changing style on task 4

--- python_variable_annotations/9-element_length.py
+++ python_variable_annotations/9-element_length.py
@@ -1,14 +1,8 @@
 #!/usr/bin/env python3
-"""
-module to return the length of elements in a list
-"""
-
-from typing import Sequence, Union, Any
-from typing import List, Tuple
+"""This module defines a function with type annotations."""
+from typing import List, Tuple, Iterable, Sequence
 
 
-def element_length(lst: Sequence[Union[int, str, Tuple[Any], List[Any]]]) -> List[int]:
-    """
-    element_length - takes a list element as argument and returns the length of the element
-    """
-    return [len(i) for i in lst]
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    """Return a list of the lengths of the elements of lst."""
+    return [(i, len(i)) for i in lst]
