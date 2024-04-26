023439c284ef7d4c99ec9abc85e85e84431ca37b
Pedro Augusto Laguna <lagunaaustore@gmail.com>
2024-04-26 11:07:08 -0400

    Fixing task1

---
+++ python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""Execute multiple coroutines at the same time with async"""
+
+import asyncio
+from typing import List
+import random
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    """Return the list of all the delays"""
+    delays = [wait_random(max_delay) for _ in range(n)]
+    return [await delay for delay in asyncio.as_completed(delays)]
+
