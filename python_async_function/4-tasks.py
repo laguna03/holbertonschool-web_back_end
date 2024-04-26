e640561e1feecb2fe12b1e42b4970d2d80f657e1
Pedro Augusto Laguna <lagunaaustore@gmail.com>
2024-04-26 11:35:07 -0400

    Task 4

---
+++ python_async_function/4-tasks.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""Tasks"""
+
+import asyncio
+import random
+import time
+from typing import List
+
+task_wait_random = __import__('3-tasks').task_wait_random
+
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    """Task_wait_n function"""
+
+    queue, array = [], []
+
+    for _ in range(n):
+        queue.append(task_wait_random(max_delay))
+
+    for q in asyncio.as_completed(queue):
+        result = await q
+        array.append(result)
+
+    return array
