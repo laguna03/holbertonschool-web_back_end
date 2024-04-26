3c378840f8082c507ac1b84ee173fcd277cfe0e2
Pedro Augusto Laguna <lagunaaustore@gmail.com>
2024-04-26 11:23:00 -0400

    Returning asyncio obj

---
+++ python_async_function/3-tasks.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""return the asyncio.Task object"""
+
+import asyncio
+import random
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+def task_wait_random(max_delay: int) -> asyncio.Task:
+    """return the asyncio.Task object"""
+    return asyncio.create_task(wait_random(max_delay))
