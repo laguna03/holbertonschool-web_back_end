b8829626a7a9ad63f810a98669e8798771470533
Pedro Augusto Laguna <lagunaaustore@gmail.com>
2024-04-26 11:10:28 -0400

    Fixing task1

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -5,10 +5,10 @@ import asyncio
 from typing import List
 import random
 
-wait_random = __import__('0-basic_async_syntax').wait_random
+wait_random = __import__("0-basic_async_syntax").wait_random
+
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
     """Return the list of all the delays"""
     delays = [wait_random(max_delay) for _ in range(n)]
     return [await delay for delay in asyncio.as_completed(delays)]
-
