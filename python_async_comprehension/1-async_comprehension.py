1875be2ef70abf56e45ff169ed15ea153fc9075a
Pedro Augusto Laguna <lagunaaustore@gmail.com>
2024-04-26 11:54:25 -0400

    First commit

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -5,10 +5,10 @@ from asyncio import sleep
 from random import uniform
 from typing import List
 
-async_generator = __import__('0-async_generator').async_generator
+async_generator = __import__("0-async_generator").async_generator
 
 
 async def async_comprehension() -> List[float]:
-    """ Async Comprehensions """
+    """Async Comprehensions"""
     a = [i async for i in async_generator()]
     return a
