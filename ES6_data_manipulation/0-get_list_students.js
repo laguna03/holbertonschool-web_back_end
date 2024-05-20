d37e348cc7c98969a5a0c474e632f8e0f968eeec
Pedro Augusto Laguna <lagunaaustore@gmail.com>
2024-05-20 17:48:18 -0400

    First commit

---
+++ ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,12 @@
+import getListStudents from './getListStudents';
+
+function getListStudents() {
+    const objects = [
+        { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+        { id: 2, firstName: 'James', location: 'Columbia' },
+        { id: 5, firstName: 'Serena', location: 'San Francisco' },
+    ];
+    return objects;
+}
+
+export default getListStudents;
