eed50f97dd9e22a9dfa64319d13eee763c7766f8
Pedro Augusto Laguna <lagunaaustore@gmail.com>
2024-05-23 15:07:37 -0400

    completing task 5

---
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -0,0 +1,12 @@
+function createInt8TypedArray(length, position, value) {
+    const buffer = new ArrayBuffer(length);
+    const view = new Int8Array(buffer, 0);
+    if (position > length - 1) {
+      throw Error('Position outside range');
+    }
+    view.setInt8(position, value);
+
+    return view;
+  }
+
+  export default createInt8TypedArray;
