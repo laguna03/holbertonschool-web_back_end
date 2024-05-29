b78163dcd8ca4eeddfbd7dad4227636a41a2cd96
Pedro Augusto Laguna <lagunaaustore@gmail.com>
2024-05-29 11:09:05 -0400

    Fixing eslint

--- ES6_data_manipulation/4-update_grade_by_city.js
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,24 +1,21 @@
 function updateStudentGradeByCity(students, city, newGrades) {
-    if (!Array.isArray(students)) {
-      return [];
-    }
-    if (!Array.isArray(newGrades)) {
-      return [];
-    }
-
-    const stCity = students.filter((student) => student.location === city);
-
-    const studentsGraded = stCity.map((student) => {
-      const gradeObj = newGrades.find((newGrade) => newGrade.studentId === student.id);
-      const grade = gradeObj ? gradeObj.grade : 'N/A';
-
-      return {
-        ...student,
-        grade,
-      };
-    });
-
-    return studentsGraded;
+  if (!Array.isArray(students) || !Array.isArray(newGrades)) {
+    return [];
   }
 
-  export default updateStudentGradeByCity;
+  const stCity = students.filter((student) => student.location === city);
+
+  const studentsGraded = stCity.map((student) => {
+    const gradeObj = newGrades.find((newGrade) => newGrade.studentId === student.id);
+    const grade = gradeObj ? gradeObj.grade : 'N/A';
+
+    return {
+      ...student,
+      grade,
+    };
+  });
+
+  return studentsGraded;
+}
+
+export default updateStudentGradeByCity;
