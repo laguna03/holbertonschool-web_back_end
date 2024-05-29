72c4646986df19861ad18770e9ac58cde3397cf9
Pedro Augusto Laguna <lagunaaustore@gmail.com>
2024-05-29 12:06:04 -0400

    Fixing eslint

--- ES6_data_manipulation/10-update_uniq_items.js
+++ ES6_data_manipulation/10-update_uniq_items.js
@@ -1,7 +1,11 @@
-const updateUniqueItems = (map) => {
-    if (!(map instanceof Map)) throw new Error('Cannot process');
-    map.forEach((value, key) => {
-      if (value === 1) map.set(key, 100);
-    });
-  };
-  export default updateUniqueItems;
+const cleanSet = (set, string) => {
+  if (!string || !string.length) return '';
+  let value = '';
+  for (const el of set) {
+    if (el && el.startsWith(string)) {
+      value += value.length === 0 ? el.replace(string, '') : el.replace(string, '-');
+    }
+  }
+  return value;
+};
+export default cleanSet;
