b3e3f7c9b75a9f87350ea92cd13232c50c38ad50
Pedro Augusto Laguna <lagunaaustore@gmail.com>
2024-05-29 21:52:08 -0400

    Fixing eslint

--- ES6_promise/5-photo-reject.js
+++ ES6_promise/5-photo-reject.js
@@ -1,5 +1,5 @@
 function uploadPhoto(filename) {
-    return Promise.reject(new Error(`${filename} cannot be processed`));
-  }
+  return Promise.reject(new Error(`${filename} cannot be processed`));
+}
 
-  export default uploadPhoto;
+export default uploadPhoto;
